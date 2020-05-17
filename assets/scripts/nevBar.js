f173323ebf77cfc8242eeb7cbdf28e8b0b48c631
AJAY <ajay.ranga.mr.unique@gmail.com>
2020-05-17 10:01:51 +0530

    Minor changes

--- assets/scripts/nevBar.js
+++ assets/scripts/nevBar.js
@@ -1,22 +1,34 @@
 const bars = document.querySelector("#bars");
-const loginButton = document.querySelectorAll("#loginButton");
-const quoteButton=document.querySelectorAll("#quoteButton");
+const list = document.querySelector(".list");
+const end = document.querySelector(".end");
 const collapse = () => {
-    if (document.querySelector(".sideBar").style.display !== "block") {
-        document.querySelector(".sideBar").style.display = "block";
+    if (window.innerWidth < 1024) {
+        if (list.classList.contains("disable")) {
+            list.classList.remove("disable");
+            end.classList.remove("disable");
+            list.classList.add("enable");
+            end.classList.add("enable");
+            setTimeout(() => {
+                list.style.display = "flex";
+                end.style.display = "flex";
+            }, 1000);
+        }
+        else if (list.classList.contains("enable")) {
+            list.classList.add("disable");
+            end.classList.add("disable");
+            list.classList.remove("enable");
+            end.classList.remove("enable");
+            setTimeout(() => {
+                list.style.display = "none";
+                end.style.display = "none";
+            }, 1000);
+        }
     }
-    else {
-        document.querySelector(".sideBar").style.display = "none";
-    }
-}
-for (var i = 0; i < loginButton.length; i++) {
-    loginButton[i].addEventListener('click', () => {
-        window.location.href = "login.html";
-    });
-}
-for (var i = 0; i < quoteButton.length; i++) {
-    quoteButton[i].addEventListener('click', () => {
-        window.location.href = "contactUs.html";
-    });
 }
 bars.addEventListener('click', collapse);
+if (window.innerWidth >= 1024) {
+    list.classList.remove("active");
+    end.classList.remove("active");
+    end.classList.remove("disable");
+    list.classList.remove("disable");
+}
