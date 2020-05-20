3fb0d31ff02ba213cabb10a962a937973bfc7104
AJAY <ajay.ranga.mr.unique@gmail.com>
2020-05-20 19:36:45 +0530

    navbar changed

--- assets/scripts/nevBar.js
+++ assets/scripts/nevBar.js
@@ -1,34 +1,16 @@
 const bars = document.querySelector("#bars");
-const list = document.querySelector(".list");
-const end = document.querySelector(".end");
-const collapse = () => {
-    if (window.innerWidth < 1024) {
-        if (list.classList.contains("disable")) {
-            list.classList.remove("disable");
-            end.classList.remove("disable");
-            list.classList.add("enable");
-            end.classList.add("enable");
-            setTimeout(() => {
-                list.style.display = "flex";
-                end.style.display = "flex";
-            }, 1000);
-        }
-        else if (list.classList.contains("enable")) {
-            list.classList.add("disable");
-            end.classList.add("disable");
-            list.classList.remove("enable");
-            end.classList.remove("enable");
-            setTimeout(() => {
-                list.style.display = "none";
-                end.style.display = "none";
-            }, 1000);
-        }
+const navBar2 = document.querySelector(".navbar2");
+bars.addEventListener('click', () => {
+    if (navBar2.style.display === "flex") {
+        navBar2.style.display = "none";
+        bars.classList.add("fa", "fa-bars");
+        bars.classList.remove("fas", "fa-times");
+        bars.style.fontSize = "1.2rem";
     }
-}
-bars.addEventListener('click', collapse);
-if (window.innerWidth >= 1024) {
-    list.classList.remove("active");
-    end.classList.remove("active");
-    end.classList.remove("disable");
-    list.classList.remove("disable");
-}
+    else {
+        navBar2.style.display = "flex";
+        bars.classList.remove("fa", "fa-bars");
+        bars.classList.add("fas", "fa-times");
+        bars.style.fontSize = "2rem";
+    }
+})
